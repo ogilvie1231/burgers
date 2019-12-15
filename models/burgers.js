a8ab0306a128c7c3e789930fcd593d15800e5e22
Alex Ogilvie <ogilvie1231@gmail.com>
2019-12-15 13:48:37 -0700

    fixing routes and devoured values.

--- models/burgers.js
+++ models/burgers.js
@@ -2,22 +2,21 @@ var orm = require("../config/orm")
 
 var burger = {
     all: function(cb) {
-      orm.all("burgers", function(res) {
-        cb(res);
-      });
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
     },
     // The variables cols and vals are arrays.
     create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
-        cb(res);
-      });
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
     },
     update: function(coloumInput, val, id, cb) {
-      orm.update("burgers", coloumInput, vals, id, function(res) {
-        cb(res);
-      });
+        orm.update("burgers", coloumInput, val, id, function(res) {
+            cb(res);
+        });
     },
-  }; 
-  
-  module.exports = burger;
-   
+};
+
+module.exports = burger;
